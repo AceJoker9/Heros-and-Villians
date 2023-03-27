9aaa7d9f87624d118ad17225ba9627d9380f7898
unknown <neilmagic25@gmail.com>
2023-03-27 13:59:53 -0400

    Added the serializeer to both apps

---
+++ hero_types/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Super_types
+
+class Super_typesSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Super_types
+        fields = ['type']
+
+        
