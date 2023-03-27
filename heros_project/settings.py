fbe166420d1bd1b169742fc42a8cb520a4c718b3
unknown <neilmagic25@gmail.com>
2023-03-27 09:02:01 -0400

    Added two apps for model creation

--- heros_project/settings.py
+++ heros_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tgvv#lgn@p0i#^u8w26=r+*=8h$_g*8f8wd8^ar1^2j@!=1=ua'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
