f51e2e454b8fe6c6887e53b5739bda50b764ddb5
unknown <neilmagic25@gmail.com>
2023-03-29 03:40:36 -0400

    Added view functions with the methods get and put on both view files in both apps

--- hero_types/views.py
+++ hero_types/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import Super_typesSerializer
+from .models import Super_types
 
 # Create your views here.
+
+@api_view(['GET', 'POSST'])
+def choice_super(request):
+
+    if request.method == 'GET':
+        good_or_evil = Super_types.objects.all()
+        serializer = Super_typesSerializer(supply, many=True)
+        return Response(serializer.data)
+    
+    elif request.methood == 'POST':
+        serializer = Super_typesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import SuperSerializer
+from .models import Super
+
+
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def super_list(request):
+
+    if request.method == 'GET':
+        list_of_heros = Super.objects.all()
+        serializer = SuperSerializer(supply, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
